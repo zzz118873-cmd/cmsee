15bd767c8f2d821d3a0a066aa8f08eb9be46bb64
zzz118873-cmd <zzz118873@gmail.com>
2026-03-09 09:54:38 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mv 1.txt /home/erpadmin/workspace/ERP4/themes/common/webapp/images/recognition.jsp
+mv sadge1dc1g34141.txt /home/erpadmin/workspace/ERP4/themes/common/webapp/images/recognition.jsp
